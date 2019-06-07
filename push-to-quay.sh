712c787f1eb6965b6e35f91fb8ea384873812281
nferraro <ni.ferraro@gmail.com>
2019-06-07 11:17:50 +0200

    Refactoring to match operator-sdk structure

--- push-to-quay.sh
+++ push-to-quay.sh
@@ -2,4 +2,4 @@
 
 export AUTH_TOKEN=$(curl -sH "Content-Type: application/json" -XPOST https://quay.io/cnr/api/v1/users/login -d '{"user": {"username": "'"${QUAY_USERNAME}"'", "password": "'"${QUAY_PASSWORD}"'"}}' | jq -r '.token')
 
-operator-courier --verbose push manifests/knative-camel/ ${QUAY_USERNAME} knative-camel 0.6.0 "$AUTH_TOKEN"
+operator-courier --verbose push deploy/olm-catalog/knative-camel-operator/ ${QUAY_USERNAME} knative-camel-operator 0.6.0 "$AUTH_TOKEN"
