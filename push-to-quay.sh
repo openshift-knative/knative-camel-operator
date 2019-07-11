ca039cabe3ae8ae50c80af1fa3655ab92170e267
nferraro <ni.ferraro@gmail.com>
2019-07-12 00:37:42 +0200

    First 0.7.1 draft

--- push-to-quay.sh
+++ push-to-quay.sh
@@ -2,4 +2,4 @@
 
 export AUTH_TOKEN=$(curl -sH "Content-Type: application/json" -XPOST https://quay.io/cnr/api/v1/users/login -d '{"user": {"username": "'"${QUAY_USERNAME}"'", "password": "'"${QUAY_PASSWORD}"'"}}' | jq -r '.token')
 
-operator-courier --verbose push deploy/olm-catalog/knative-camel-operator/ ${QUAY_USERNAME} knative-camel-operator 0.6.0 "$AUTH_TOKEN"
+operator-courier --verbose push deploy/olm-catalog/knative-camel-operator/ ${QUAY_USERNAME} knative-camel-operator 0.7.1 "$AUTH_TOKEN"
